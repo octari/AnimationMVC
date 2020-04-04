059f82dd79347214a04a3a8bf076cc94c18d570a
octari <ycjariel@gmail.com>
2020-04-04 18:02:18 -0500

    a testOutPutFile

--- code/test/TextualTest.java
+++ code/test/TextualTest.java
@@ -1,5 +1,6 @@
 
 import org.junit.Before;
+import org.junit.Test;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -17,13 +18,14 @@ import cs5004.model.ReadOnlyModel;
 import cs5004.model.ScaleChange;
 import cs5004.model.Shape;
 import cs5004.model.ShapeType;
+import cs5004.view.TextualView;
 
 import static org.junit.Assert.assertEquals;
 /**
  *
  */
 public class TextualTest {
-  ReadOnlyModel model;
+  AnimatorImpl model;
   Rectangle rectangle;
   Oval oval;
   Position p1;
@@ -32,16 +34,15 @@ public class TextualTest {
   Position p4;
   PosChange changeP1;
   PosChange changeP2;
-  PosChange changeP3;
   ColorChange changeColor1;
   ScaleChange changeSize1;
-  TextualTest textualView;
+  TextualView textualView;
 
   ArrayList<Shape> shapesList;
 
   @Before
   public void setup() {
-    model = new ReadOnlyModel;
+    model = new AnimatorImpl();
     p1 = new Position(250, 270);
     p2 = new Position(390, 390);
     p3 = new Position(510, 115);
@@ -55,12 +56,33 @@ public class TextualTest {
     changeP1 = new PosChange("R", 10, 50, p2);
     changeP2 = new PosChange("O", 10, 50, p1);
 
-
     changeColor1 = new ColorChange("O", 50, 80, 10, 0, 0);
     changeSize1 = new ScaleChange("O", 61, 70, 50, 100);
 
-    model.addShape(rectangle);
-    model.addShape(oval);
+    model.addShape(rectangle.getId(), rectangle.getType(), rectangle.getAppear(),
+            rectangle.getDisappear(), rectangle.getPos(), rectangle.getWidth(),
+            rectangle.getHeight(), rectangle.getR(), rectangle.getG(), rectangle.getB());
+    model.addShape(oval.getId(), oval.getType(), oval.getAppear(), oval.getDisappear(),
+            oval.getPos(), oval.getRadius1(), oval.getRadius2(), oval.getR(), oval.getG(),
+            oval.getB());
+
+    model.addMove("R", changeP1.getStartTime(), changeP1.getStartTime(), changeP1.getEndPos());
+    model.addScale("R", changeSize1.getStartTime(),changeSize1.getEndTime(),
+            changeSize1.getEndIndex1(), changeSize1.getEndIndex2());
+    model.addColor("R", changeColor1.getStartTime(), changeColor1.getEndTime(),
+            changeColor1.getEndR(), changeColor1.getEndG(), changeColor1.getEndB());
+
+    textualView = new TextualView(model, "out");
+
+
+  }
+
+  /**
+   * Test for getTextDescription() method.
+   */
+  @Test
+  public void testOutputFile () {
+    assertEquals("", this.textualView.getCurrentState());
   }
 
 
